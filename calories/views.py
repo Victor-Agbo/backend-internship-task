a02afa282768910532e2e827dd6f8ed68e0b5352
Victor-Agbo <victoragbo72@gmail.com>
2023-06-16 18:35:11 +0530

    Added Login and Register functions

--- calories/views.py
+++ calories/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from . import models
 
 # Create your views here.
 
 
+@login_required
 def index(request):
     return render(request, "index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "login.html")
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register_view(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "register.html")
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(
+                request, "register.html", {"message": "Passwords must match."}
+            )
+
+        # Attempt to create new user
+        try:
+            user = models.User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(
+                request, "register.html", {"message": "Username already taken."}
+            )
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "register.html")
--- dive/settings.py
+++ dive/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     "calories",
 ]
 
+LOGIN_URL = "/login"
+LOGIN_REDIRECT_URL = "/login"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = "calories.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
